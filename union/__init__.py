d22297e9c1d032656f8d448f259a3278d989bb9d
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2024-03-02 16:36:08 -0600

    Export symbols

--- union/__init__.py
+++ union/__init__.py
@@ -1,3 +1,11 @@
 from union import result, option, util
 
 _, _ = result, option
+
+from union.result import Result as Result
+from union.result import Ok as Ok
+from union.result import Err as Err
+
+from union.option import Option as Option
+from union.option import Just as Just
+from union.option import Nothing as Nothing
